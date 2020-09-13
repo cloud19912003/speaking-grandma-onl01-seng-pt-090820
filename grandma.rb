67cee1902d5581c2fb87dc2e69846bec5d33b50f
Michael Murray <cloud19912003@live.com>
2020-09-13 19:49:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,8 +16,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    print "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   else
-    print "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
